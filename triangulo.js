2991b33e9dc8c49737cbcc1716b16da4821f9728
batatacom <113316376+batatacom@users.noreply.github.com>
2023-10-18 09:59:36 -0300

    18.10

---
+++ triangulo.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form")
+const resp = document.querySelector("h3")
+("#outResp")
+const resp1 =- document.querySelector
+("#outResp1")
+
+form.addEventListener ("submit", (e) =>{
+    e.preventDefault()
+
+    const ladoA = number(form.inladoA.value)
+    const ladoB = number(form.inladoB.value)
+    const ladoC = number(form.inladoC.value)
+
+    if((ladoA > ladoB + ladoC) || (ladoB > ladoA + ladoC) || (ladoC > ladoA + ladoB)){
+        alert("As medidas não formam um triângulo pois um dos lados não pode ser maior que a soma dos outro dois.")
+        form.inladoA.focus()
+        form.inladoB.focus()
+        form.inladoC.focus()
+        return
+    }
+
+    resp.innerText = 'Lados podem formam um triângulo'
+
+    if(ladoA == ladoB && ladoA == ladoC){
+        resp1.innerText = 'Tipo: Equilátero'
+    }else if((ladoA == ladoB) || (ladoA == ladoC) || (ladoB == ladoC)){
+        resp1.innerText = 'Tipo: Isóceles'   
+    }else{
+        resp1.innerText = 'Tipo: Escaleno'
+    }
+}) 
