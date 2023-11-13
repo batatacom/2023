477204af0e6b71201a4429026d4f2f996c6c09e9
batatacom <113316376+batatacom@users.noreply.github.com>
2023-11-13 08:06:59 -0300

    13.11

--- peso.js
+++ peso.js
@@ -1,21 +1,20 @@
-const form = document.querySelector("fora")
+const form = document.querySelector("form")
 const resp = document.querySelector("h3")
 
 form.addEventListener("submit", (e)=> {
    e.preventDefault()
 
    const nome = form.innome.value
-   const masculino = fom.inmasculino.checked
-   const altura = number (form.inaltura.value)
+   const masculino = form.inmasculino.checked
+   const altura = Number(form.inaltura.value)
 
-   let perso
+   let peso
    if (masculino) {
     peso = 22 * Math.pow(altura, 2)
    } else {
     peso = 21 * Math.pow(altura, 2)
    }
-
-   resp.innertext = '$ {nome} seu peso ideal de é: ${peso.tofixed(3)kg' 
+   resp.innerText = `${nome} seu peso ideal de é: ${peso.toFixed(3)}kg`
 })
 
 
